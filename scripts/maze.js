8609e40e31621c6fc147081b91c5ea2618ea59ac
chepeniv <jose.olmos@atlasschool.com>
2024-12-06 19:35:37 -0600

    defined preliminary maze block codes

--- scripts/maze.js
+++ scripts/maze.js
@@ -3,10 +3,23 @@
 
 const mazeSize = 16;
 
+// key codes
 const left = 37;
 const up = 38;
 const right = 39;
 const down = 40;
+
+// block type definitions
+const horHall = 0;
+const vertHall = 1;
+const deadEndDown = 2;
+const deadEndLeft = 3;
+const deadEndUp = 4;
+const deadEndRight = 5;
+const cornerUpLeft = 6;
+const cornerUpRight = 7;
+const cornerDownRight = 8;
+const cornerDownLeft = 9;
 
 function buildRow (size, i) {
   $('.maze').append(`<tr class="row ${i}"></tr>`);
@@ -21,6 +34,7 @@ function buildMazeGrid (size) {
   }
 }
 
+// create random entry point on left edge
 function setEntry (size) {
   const entry = Math.floor(Math.random() * size);
   const row = `.${entry} .0`;
