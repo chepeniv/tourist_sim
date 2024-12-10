7498aa9b84853c33cbe82ab1e18e5a2e3d127289
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-12-09 21:18:01 -0600

    M - 9:17 PM - updated the conversion code again, still not perfect

--- maze_generator.py
+++ maze_generator.py
@@ -85,32 +85,34 @@ def maze_to_json(maze):
     def cell_to_number(cell):
         walls = cell.walls.copy()
 
-        if walls['top'] and walls['bottom']:
-            if walls['left'] and walls['right']:
-               return 3  # All walls
-            elif walls['left']:
-                return 4  # Top and bottom, left
-            elif walls['right']:
-                return 5  # Top and bottom, right
-            else:
-                return 0  # Top and bottom only
-        elif walls['left'] and walls['right']:
-            if walls['top']:
-                return 6  # Left and right, top
-            elif walls['bottom']:
-                return 7  # Left and right, bottom
-            else:
-                return 1  # Left and right only
-        elif walls['top']:
-            return 2  # Top only
-        elif walls['bottom']:
-            return 8  # Bottom only
-        elif walls['left']:
-            return 9  # Left only
-        elif walls['right']:
-            return 10  # Right only
-        else:
-            return 11  # No walls
+        if walls['top'] and walls['bottom'] and not walls['left'] and not walls['right']:
+            return 0
+        elif walls['left'] and walls['right'] and not walls['top'] and not walls['bottom']:
+            return 1
+        elif walls['left'] and walls['right'] and walls['top'] and not walls['bottom']:
+            return 2
+        elif walls['top'] and walls['bottom'] and walls['right'] and not walls['left']:
+            return 3
+        elif walls['left'] and walls['top'] and walls['bottom'] and not walls['right']:
+            return 4
+        elif walls['left'] and walls['right'] and walls['bottom'] and not walls['top']:
+            return 5
+        elif walls['top'] and walls['left'] and not walls['bottom'] and not walls['right']:
+            return 6
+        elif walls['top'] and walls['right'] and not walls['bottom'] and not walls['right']:
+            return 7
+        elif walls['bottom'] and walls['left'] and not walls['top'] and not walls['right']:
+            return 8
+        elif walls['bottom'] and walls['right'] and not walls['top'] and not walls['left']:
+            return 9
+        elif walls['top'] and not walls['right'] and not walls['bottom'] and not walls['left']:
+            return 10
+        elif walls['right'] and not walls['top'] and not walls['left'] and not walls['bottom']:
+            return 11
+        elif walls['bottom'] and not walls['left'] and not walls['top'] and not walls['right']:
+            return 12
+        elif walls['left'] and not walls['top'] and not walls['right'] and not walls['bottom']:
+            return 13
 
     return [[cell_to_number(cell) for cell in row] for row in maze]
 
