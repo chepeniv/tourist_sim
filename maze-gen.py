276495708cf1962f2a15849c59b43fe70ae48dbb
chepeniv <jose.olmos@atlasschool.com>
2024-12-10 13:15:04 -0600

    changed encoding scheme and mapped tables

--- maze-gen.py
+++ maze-gen.py
@@ -3,29 +3,25 @@
 from random import randint, choice
 
 maze_size = 12
-wall_codes = {
-    # singles
-    'no_wall':  0,
-    'left':     1,
-    'top':      2,
-    'right':    3,
-    'bot':      4,
 
-    # halls
-    'l_r':      5,
-    't_b':      6,
+(none, left, right, left_right) = (0, 1, 2, 3)
+(top, bottom, top_bottom) = (1, 2, 3)
 
-    # dead-ends
-    'r_t_b':    7,
-    'r_t_b':    8,
-    'r_t_b':    9,
-    'r_t_b':    10,
+# codes indicate which walls are _present_
+wall_codes = [
+    # x,  l,  r, lr
+    [ 0,  1,  2,  3], # x
+    [ 4,  5,  6,  7], # t
+    [ 8,  9, 10, 11], # b
+    [12, 13, 14, 15], # tb
+]
 
-    # corners
-    'l_t':      11,
-    'r_t':      12,
-    'r_b':      13,
-    'l_b':      14,
+wall_indeces = {
+            # prev      next
+    'up':    (bottom,   top),
+    'right': (left,     right),
+    'down':  (top,      bottom),
+    'left':  (right,    left)
 }
 
 traceback_path = list()
@@ -36,7 +32,7 @@ def init_maze(size):
     maze_row = []
 
     for x in range(size):
-        maze_row.append(0)
+        maze_row.append([3, 3]) # all blocks closed
     for x in range(size):
         maze_array.append(maze_row)
 
@@ -61,7 +57,6 @@ def next_xy(last_pos):
 
     available = []
     for item in directions.items():
-        key = item[0]
         xy = item[1]
         (newy, newx) = xy
 
@@ -76,17 +71,34 @@ def next_xy(last_pos):
         return traceback_path.pop()
 
     traceback_path.append(last_pos)
-    (path, block) = choice(available)
-    # print(path + '\t: ' + str(block))
-    return block
+    return choice(available)
 
-def draw_path():
-    current_pos = (0, 0)
-    current_pos = next_xy(current_pos)
-    while current_pos != (0, 0):
-        last_pos = current_pos
-        current_pos = next_xy(current_pos)
-    print(len(visited_blocks))
+def draw_path(maze):
+    init_pos = (0, 0)
+    next_pos = next_xy(init_pos)
+    while next_pos != (0, 0):
+        last_pos = next_pos
 
-# maze = init_maze(maze_size)
-draw_path()
+        (path, next_pos) = next_xy(last_pos)
+        (prev_idx, next_idx) = wall_indeces.get(path)
+
+        last_y = last_pos[0]
+        last_x = last_pos[1]
+        next_y = next_pos[0]
+        next_x = next_pos[1]
+
+        # solution / simplification the trace-back
+        # will automatically remove the remaining half-wall
+        (last_tb, last_lr) = maze[last_y][last_x]
+        (next_tb, next_lr) = maze[next_y][next_x]
+
+        if path in ('up, down'):
+            maze[last_y][last_x] = (prev_idx, last_lr)
+            maze[next_y][next_x] = (next_idx, next_lr)
+        else:
+            maze[last_y][last_x] = (last_tb, prev_idx)
+            maze[next_y][next_x] = (next_tb, next_idx)
+
+maze = init_maze(maze_size)
+draw_path(maze)
+print_maze(maze)
